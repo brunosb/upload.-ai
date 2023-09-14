4f9e9b6efd7553c49e72090515884f4c1f052324
Bruno Silva Barbosa <barbosa.developer@gmail.com>
2023-09-14 12:26:35 -0300

    api for transcription and completition openai

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,10 +1,20 @@
 import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAiCompletionRoute } from './routes/generate-ai-completion'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello World'
+app.register(fastifyCors, {
+  origin: '*',
 })
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app
   .listen({
