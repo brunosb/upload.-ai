6922565eb2aef060f441357ec5b488e166b404b5
Bruno Silva Barbosa <barbosa.developer@gmail.com>
2023-09-16 21:25:41 -0300

    convertendo video em audio; integrando com backend; completion com ai

--- upload-ai-api/src/routes/generate-ai-completion.ts
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -1,17 +1,18 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
+import { streamToResponse, OpenAIStream } from 'ai'
 import { openai } from '../lib/openai'
 
 export async function generateAiCompletionRoute(app: FastifyInstance) {
   app.post('/ai/complete', async (request, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     })
 
-    const { videoId, temperature, template } = bodySchema.parse(request.body)
+    const { videoId, temperature, prompt } = bodySchema.parse(request.body)
 
     const video = await prisma.video.findFirstOrThrow({
       where: {
@@ -25,17 +26,22 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
         .send({ error: 'Video transcription was not generated yet' })
     }
 
-    const promptMessage = template.replace(
-      '{transcription}',
-      video.transcription,
-    )
+    const promptMessage = prompt.replace('{transcription}', video.transcription)
 
     const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-3.5-turbo-16k',
       temperature,
       messages: [{ role: 'user', content: promptMessage }],
+      stream: true,
     })
 
-    return response
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      },
+    })
   })
 }
